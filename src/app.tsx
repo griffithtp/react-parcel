5e60efbcde84481dc0b14180a58ea91fb2299baf
Griffithtp <griffithtp@gmail.com>
2020-02-11 23:45:44 +0000

    initial setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './lib/button'
+
+export default function (props: any) {
+  return (
+    <div>
+      app with parcel
+      <Button>
+        Click me
+      </Button>
+    </div>
+  )
+}
---
+++ src/lib/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function(prop: any) {
+  return (
+    <button>Button {prop.children}</button>
+  )
+}
